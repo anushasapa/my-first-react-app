c9bff424ba625e6ac4f1ba24a35567f5c0c75195
asapa <asapa@deloitte.com>
2019-11-11 21:25:39 -0500

    Updates

---
+++ precache-manifest.233baab61fea03055b649d034d8dfd26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ceb35f7c9d9a6e00501fc230ed982e1",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "5f998b56e8081823f01f",
+    "url": "/my-first-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36eb5e0a6679f037e322",
+    "url": "/my-first-react-app/static/js/2.1c421565.chunk.js"
+  },
+  {
+    "revision": "5f998b56e8081823f01f",
+    "url": "/my-first-react-app/static/js/main.25266cd8.chunk.js"
+  },
+  {
+    "revision": "b7977767f8fbbeef333c",
+    "url": "/my-first-react-app/static/js/runtime-main.ca13226b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-first-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
